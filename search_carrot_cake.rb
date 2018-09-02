420e0f3f2d0760072e773b680314af1fce36cc59
Jonas Porto <jporto.contato@hotmail.com>
2018-09-02 11:44:07 -0300

    quick watir demo

---
+++ search_carrot_cake.rb
@@ -0,0 +1,22 @@
+require 'watir'
+require 'webdrivers'
+
+url = 'https://tudogostoso.com.br'
+
+# Start a browser session with URL
+browser = Watir::Browser.start url
+
+# Find the form search element by ID
+form_search = browser.element id: 'search'
+
+# Find the input element
+input = form_search.text_field class: 'txt'
+
+# Fill the input with the search term
+input.set 'bolo de cenoura'
+
+# Find the submit button
+submit = form_search.element type: 'submit'
+
+# Click in the Submit button
+submit.click
